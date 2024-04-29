065570ade8fecb9d6539698a1f0aaed8224adcf3
Levchenko Nikita <n.levchenko@everypixel.ru>
2024-04-29 20:16:53 +0500

    BSC-86 test 9

--- examples/transfer34.js
+++ examples/transfer34.js
@@ -1,4 +1,4 @@
-// import {Apis} from "bitsharesjs-ws";
+import {Apis} from "bitsharesjs-ws";
 import {ChainStore, FetchChain, PrivateKey, TransactionHelper, Aes, TransactionBuilder} from "../lib";
 
 console.log("... test instance34 88 11000000000000000000");
@@ -13,74 +13,74 @@ var from_account = "nathan"
 var to_account = "oliver23"
 var memo_text = "Your memo goes in here.."
 
-// let pKeyActive = PrivateKey.fromWif("5KGQRZ2jUv6SpMkFuhm8nxrGfRF9GFZMAjMzJrpoArJv57iANtx");  // Replace with your own Active Private Key
-// let pKeyMemo = PrivateKey.fromWif("5KGQRZ2jUv6SpMkFuhm8nxrGfRF9GFZMAjMzJrpoArJv57iANtx");  // Replace with your own Memo Private Key
+let pKeyActive = PrivateKey.fromWif("5KGQRZ2jUv6SpMkFuhm8nxrGfRF9GFZMAjMzJrpoArJv57iANtx");  // Replace with your own Active Private Key
+let pKeyMemo = PrivateKey.fromWif("5KGQRZ2jUv6SpMkFuhm8nxrGfRF9GFZMAjMzJrpoArJv57iANtx");  // Replace with your own Memo Private Key
 
-console.log("... test instance34 88 00");
+console.log("... test instance34 88 00 Apis.instance");
 
-// Apis.instance(witness_node, true).init_promise.then(res => {
-//    console.log("connected to:", res[0].network);
+Apis.instance(witness_node, true).init_promise.then(res => {
+   console.log("connected to:", res[0].network);
 
-    // ChainStore.init(nobroadcast).then(() => {
+    ChainStore.init(nobroadcast).then(() => {
 
-    //     let fromAccount = from_account;
-    //     let memoSender = fromAccount;
-    //     let memo = memo_text;
+        let fromAccount = from_account;
+        let memoSender = fromAccount;
+        let memo = memo_text;
 
-    //     let toAccount = to_account;
+        let toAccount = to_account;
 
-    //     let sendAmount = {
-    //         amount: amount_to_send,
-    //         asset: asset_to_send
-    //     }
+        let sendAmount = {
+            amount: amount_to_send,
+            asset: asset_to_send
+        }
 
-    //     Promise.all([
-    //             FetchChain("getAccount", fromAccount),
-    //             FetchChain("getAccount", toAccount),
-    //             FetchChain("getAccount", memoSender),
-    //             FetchChain("getAsset", sendAmount.asset),
-    //             FetchChain("getAsset", sendAmount.asset)
-    //         ]).then((res)=> {
-    //             // console.log("got data:", res);
-    //             let [fromAccount, toAccount, memoSender, sendAsset, feeAsset] = res;
+        Promise.all([
+                FetchChain("getAccount", fromAccount),
+                FetchChain("getAccount", toAccount),
+                FetchChain("getAccount", memoSender),
+                FetchChain("getAsset", sendAmount.asset),
+                FetchChain("getAsset", sendAmount.asset)
+            ]).then((res)=> {
+                // console.log("got data:", res);
+                let [fromAccount, toAccount, memoSender, sendAsset, feeAsset] = res;
 
-    //             // Memos are optional, but if you have one you need to encrypt it here
-    //             let memoFromKey = memoSender.getIn(["options","memo_key"]);
-    //             console.log("memo pub key:", memoFromKey);
-    //             let memoToKey = toAccount.getIn(["options","memo_key"]);
-    //             let nonce = TransactionHelper.unique_nonce_uint64();
+                // Memos are optional, but if you have one you need to encrypt it here
+                let memoFromKey = memoSender.getIn(["options","memo_key"]);
+                console.log("memo pub key:", memoFromKey);
+                let memoToKey = toAccount.getIn(["options","memo_key"]);
+                let nonce = TransactionHelper.unique_nonce_uint64();
 
-    //             let memo_object = {
-    //                 from: memoFromKey,
-    //                 to: memoToKey,
-    //                 nonce,
-    //                 message: Aes.encrypt_with_checksum(
-    //                     pKeyMemo,
-    //                     memoToKey,
-    //                     nonce,
-    //                     memo
-    //                 )
-    //             }
+                let memo_object = {
+                    from: memoFromKey,
+                    to: memoToKey,
+                    nonce,
+                    message: Aes.encrypt_with_checksum(
+                        pKeyMemo,
+                        memoToKey,
+                        nonce,
+                        memo
+                    )
+                }
 
-    //             let tr = new TransactionBuilder()
+                let tr = new TransactionBuilder()
 
-    //             tr.add_type_operation( "transfer", {
-    //                 delegated_balance_id: null,
-    //                 fee: {
-    //                     amount: 0,
-    //                     asset_id: feeAsset.get("id")
-    //                 },
-    //                 from: fromAccount.get("id"),
-    //                 to: toAccount.get("id"),
-    //                 amount: { amount: sendAmount.amount, asset_id: sendAsset.get("id") },
-    //                 memo: memo_object
-    //             } )
+                tr.add_type_operation( "transfer", {
+                    delegated_balance_id: null,
+                    fee: {
+                        amount: 0,
+                        asset_id: feeAsset.get("id")
+                    },
+                    from: fromAccount.get("id"),
+                    to: toAccount.get("id"),
+                    amount: { amount: sendAmount.amount, asset_id: sendAsset.get("id") },
+                    memo: memo_object
+                } )
 
-    //             tr.set_required_fees().then(() => {
-    //                 tr.add_signer(pKeyActive, pKeyActive.toPublicKey().toPublicKeyString());
-    //                 console.log("serialized transaction:", tr.serialize());
-    //                 tr.broadcast();
-    //             })
-    //         });
-    // });
-// });
+                tr.set_required_fees().then(() => {
+                    tr.add_signer(pKeyActive, pKeyActive.toPublicKey().toPublicKeyString());
+                    console.log("serialized transaction:", tr.serialize());
+                    tr.broadcast();
+                })
+            });
+    });
+});
